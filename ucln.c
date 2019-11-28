9878b562b6072b9a8d7eb34aa5b85d279b52a431
Skick <skick1234@gmail.com>
2019-11-28 15:15:57 +0700

    28/11/2019

--- ucln.c
+++ ucln.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-unsigned int ucln(unsigned int x, unsigned int y);
+int ucln(int x, int y) {
+	return (y == 0) ? x : ucln(y, x%y);
+}
 
 int main() {
-	unsigned int a, b;
+	int a, b;
 	printf("Tinh UCLN(a, b)\nNhap a, b: ");
 	scanf("%u %u", &a, &b);
 	printf("UCLN(%u, %u) = %u", a, b, ucln(a, b));
 	return 0;
-}
-
-unsigned int ucln(unsigned int x, unsigned int y) {
-	if (x == y) return x;
-	else if (x > y) return ucln(x-y, y);
-	else return ucln(x, y-x);
 }
