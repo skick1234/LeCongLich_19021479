34e1089754c3ead044565795adec2b73e5972cae
Skick <skick1234@gmail.com>
2020-03-17 19:18:44 +0700

    From Skick with <3

--- 2. LTNC/19021497-W2-01.cpp
+++ 2. LTNC/19021497-W2-01.cpp
@@ -2,36 +2,21 @@
 
 using namespace std;
 
-// Complete the findDigits function below.
-int findDigits(int n) {
-    int x = 0, num = n;
-    while (n >= 1) {
-        int d = n % 10;
-        n /= 10;
-        if (d && num % d == 0) x++;
-    }
-    return x;
-}
-
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
     int t;
     cin >> t;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    for (int t_itr = 0; t_itr < t; t_itr++) {
+    for (int i = 0; i < t; i++) {
         int n;
         cin >> n;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-        int result = findDigits(n);
-
-        fout << result << "\n";
+        int x = 0, num = n;
+        while (n >= 1) {
+            int d = n % 10;
+            n /= 10;
+            if (d && num % d == 0) x++;
+        }
+        cout << x << endl;
     }
-
-    fout.close();
-
     return 0;
 }
--- 2. LTNC/19021497-W2-04.cpp
+++ 2. LTNC/19021497-W2-04.cpp
@@ -2,11 +2,6 @@
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-// Complete the migratoryBirds function below.
 int migratoryBirds(vector<int> arr) {
     int a[6] = {};
     for (int x: arr) a[x]++;
@@ -18,70 +13,15 @@ int migratoryBirds(vector<int> arr) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string arr_count_temp;
-    getline(cin, arr_count_temp);
-
-    int arr_count = stoi(ltrim(rtrim(arr_count_temp)));
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split(rtrim(arr_temp_temp));
-
-    vector<int> arr(arr_count);
-
-    for (int i = 0; i < arr_count; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
+    int n, a[6] = {}, tmp;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        a[tmp]++;
     }
-
-    int result = migratoryBirds(arr);
-
-    fout << result << "\n";
-
-    fout.close();
-
+    a[0] = 1;
+    for (int i = 2; i <= 5; i++)
+        if (a[i] > a[a[0]]) a[0] = i;
+    cout << a[0];
     return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
