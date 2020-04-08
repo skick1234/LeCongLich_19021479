4dbece4172a41a3065932d13f4e92337fe4a1ff5
Skick <skick1234@gmail.com>
2020-04-08 15:03:22 +0700

    From Skick with <3

--- 2. LTNC/8-4/BT2.cpp
+++ 2. LTNC/8-4/BT2.cpp
@@ -10,15 +10,15 @@ int main() {
 
     int n, input, mi, ma, s = 0, c = 0;
     inp >> n;
-    vector<int> a(10);
+    vector<int> v(10, 0);
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < n; i++) {
         inp >> input;
-        a[input]++;
+        v[input]++;
     }
 
     for (int i = 0; i < 10; i++)
-        out << i << ':' << a[i] << endl;
+        out << i << ':' << v.at(i) << endl;
 
     inp.close();
     out.close();
