622b14ef7971ae98f3191d0f6b7e3477eed6e984
Skick <skick1234@gmail.com>
2020-04-08 10:24:04 +0700

    From Skick with <3

--- 2. LTNC/8-4/2.cpp
+++ 2. LTNC/8-4/2.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <fstream>
-#include <algorithm>
 using namespace std;
 
 int main() {
@@ -11,20 +10,11 @@ int main() {
 
     int n, input, mi, ma, s = 0, c = 0;
     inp >> n;
-    vector<int> a;
+    vector<int> a(10);
     for (int i = 0; i < n; i++) {
         inp >> input;
         a.push_back(input);
     }
-
-    for (int i = 0; i < n; i++) {
-        input = a.at(i);
-        mi = (i == 0 ? input : min(input, mi));
-        ma = (i == 0 ? input : max(input, ma));
-        if (input % 2) c++;
-        else s += input;
-    }
-
     out << mi << endl << ma << endl << s << endl << c;
 
     inp.close();
