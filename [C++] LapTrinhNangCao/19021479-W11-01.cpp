9c77b6881e5ac6edafd0f5d598916720686415dd
Skick <skick1234@gmail.com>
2020-05-13 20:51:32 +0700

    C++98 compatible

--- [C++] LapTrinhNangCao/19021479-W11-01.cpp
+++ [C++] LapTrinhNangCao/19021479-W11-01.cpp
@@ -2,6 +2,12 @@
 #include <sstream>
 using namespace std;
 
+string NumberToString (int Number ) {
+    ostringstream ss;
+    ss << Number;
+    return ss.str();
+}
+
 class Student {
     int age, standard;
     string first_name, last_name;
@@ -31,27 +37,27 @@ class Student {
             return first_name;
         }
         string to_string() {
-            return std::to_string(age) + "," + first_name + "," + last_name + "," + std::to_string(standard);
+            return NumberToString(age) + "," + first_name + "," + last_name + "," + NumberToString(standard);
         }
 };
 
 int main() {
     int age, standard;
     string first_name, last_name;
-    
+
     cin >> age >> first_name >> last_name >> standard;
-    
+
     Student st;
     st.set_age(age);
     st.set_standard(standard);
     st.set_first_name(first_name);
     st.set_last_name(last_name);
-    
+
     cout << st.get_age() << "\n";
     cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
     cout << st.get_standard() << "\n";
     cout << "\n";
     cout << st.to_string();
-    
+
     return 0;
 }
